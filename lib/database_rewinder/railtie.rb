0f97feba2c67d698b55daeb89ff695fa97b9c487
Yutaka HARA <yutaka.hara+github@gmail.com>
2014-02-28 16:15:19 +0900

    Fix requiring 'rails' unconditionally
    require 'rails' raises LoadError in a non-rails project.

--- lib/database_rewinder/railtie.rb
+++ lib/database_rewinder/railtie.rb
@@ -1,11 +1,15 @@
-require 'rails'
-
-module DatabaseRewinder
-  class Railtie < ::Rails::Railtie
-    initializer 'database_rewinder', after: 'active_record.initialize_database' do
-      ActiveSupport.on_load :active_record do
-        DatabaseRewinder.init
-        require_relative 'active_record_monkey'
+begin
+  require 'rails'
+rescue LoadError
+  # This is a non-rails project (i.e. the user did not have rails in Gemfile)
+else
+  module DatabaseRewinder
+    class Railtie < ::Rails::Railtie
+      initializer 'database_rewinder', after: 'active_record.initialize_database' do
+        ActiveSupport.on_load :active_record do
+          DatabaseRewinder.init
+          require_relative 'active_record_monkey'
+        end
       end
     end
   end
